9020d74989c655a6cf607418d0a086580416ab71
maxd2098 <maxd2098@gmail.com>
2022-10-27 19:11:13 +1000

    Init

---
+++ src/main/java/plugin/atb/booking/BookingApplication.java
@@ -0,0 +1,13 @@
+package plugin.atb.booking;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class BookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingApplication.class, args);
+    }
+
+}
