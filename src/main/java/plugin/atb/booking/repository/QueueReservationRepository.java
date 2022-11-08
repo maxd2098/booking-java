442106b67bd0f0fd37685caecf7197c975d96df7
maxd2098 <maxd2098@gmail.com>
2022-11-08 14:50:33 +1000

    Создана миграция, сущность и репозиторий для таблицы employee

--- src/main/java/plugin/atb/booking/repository/QueueReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package plugin.atb.booking.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import plugin.atb.booking.model.QueueReservation;
-
-public interface QueueReservationRepository extends CrudRepository<QueueReservation, Long> {
-}
--- src/main/resources/db/migration/V2__CREATE_TABLE_WORKPLACE.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE work_place
-(
-    id_work_place BIGSERIAL PRIMARY KEY,
-    type BOOLEAN,
-    num_seats INTEGER,
-    num_level INTEGER,
-    info TEXT
-)
