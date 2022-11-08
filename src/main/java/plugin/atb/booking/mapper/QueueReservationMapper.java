cbeaed3f3c2a91f9f84e43a6376a9349563c0537
maxd2098 <maxd2098@gmail.com>
2022-11-08 16:02:14 +1000

    Созданы сервис, mapper, необходимые для работы dto и контроллер для таблицы queue_reservation

---
+++ src/main/java/plugin/atb/booking/mapper/QueueReservationMapper.java
@@ -0,0 +1,27 @@
+package plugin.atb.booking.mapper;
+
+
+import plugin.atb.booking.dto.QueueReservationDto;
+import plugin.atb.booking.model.QueueReservation;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class QueueReservationMapper {
+
+    public static QueueReservation mapDtoToQueueReservation(QueueReservationDto queueReservationDto,
+                                                            Long idEmployee,
+                                                            Long idWorkPlace) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime timeBegin = LocalTime.parse(queueReservationDto.getTimeBegin(), formatter);
+        LocalTime timeEnd = LocalTime.parse(queueReservationDto.getTimeEnd(), formatter);
+        return new QueueReservation(queueReservationDto.getDate(),
+                timeBegin,
+                timeEnd,
+                queueReservationDto.isAdminPermission(),
+                idEmployee,
+                idWorkPlace
+        );
+    }
+
+}
